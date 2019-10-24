11a0ec64d4857762b7074458b4ddad46a6777171
Name <Email>
2019-10-24 18:23:50 +0300

    debug

---
+++ precache-manifest.ace657efd5c7a7eed0dcec62e14cf766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f31a91dcde966a47b9e32e2e958b46",
+    "url": "/Todo-with-server/index.html"
+  },
+  {
+    "revision": "a43fb56952262d09b72c",
+    "url": "/Todo-with-server/static/css/2.8bed955a.chunk.css"
+  },
+  {
+    "revision": "f08bdf8ce799e8b2b697",
+    "url": "/Todo-with-server/static/css/main.1aa05e05.chunk.css"
+  },
+  {
+    "revision": "a43fb56952262d09b72c",
+    "url": "/Todo-with-server/static/js/2.cf5d89de.chunk.js"
+  },
+  {
+    "revision": "f08bdf8ce799e8b2b697",
+    "url": "/Todo-with-server/static/js/main.1520ee40.chunk.js"
+  },
+  {
+    "revision": "5d4e6e020b81bf935477",
+    "url": "/Todo-with-server/static/js/runtime-main.0d76c1c4.js"
+  },
+  {
+    "revision": "ca838bd200c1750bca27399e9ff8d068",
+    "url": "/Todo-with-server/static/media/crown.ca838bd2.png"
+  }
+]);
